b3f788f361abd8059c654522e258ccb6b48adad2
CodeMat <codemat.biz@gmail.com>
2023-03-06 23:27:53 +0100

    updated

--- pages/components/BannerCard.js
+++ pages/components/BannerCard.js
@@ -1,18 +1,38 @@
-import { Box } from "@chakra-ui/react";
+import { AspectRatio } from "@chakra-ui/react";
 import Image from "next/image";
 
 const BannerCard = ({ banner }) => {
   return (
-      <Box
-        mb='8'
-        mx={{ md: 1 }}
-        display='flex'
-        justifyContent='center'
-        rounded={{ md: "md" }}
-        overflow='hidden'>
-        <Image alt='banner' src={banner} width={1500} height={566} />
-      </Box>
-  
+    <AspectRatio
+      ratio={4 / 3}
+      maxH='60vh'
+      mx={{ md: 1 }}
+      rounded={{ md: "md" }}
+      overflow='hidden'>
+      <Image
+        alt='banner'
+        src={banner}
+        layout='fill'
+        // width={1500} height={566}
+      />
+    </AspectRatio>
+    // <Box
+    //   pos='relative'
+    //   mb='8'
+    //   maxW='full'
+    //   maxH='70vh'
+    //   mx={{ md: 1 }}
+    //   display='flex'
+    //   justifyContent='center'
+    //   rounded={{ md: "md" }}
+    //   overflow='hidden'>
+    //   <Image
+    //     alt='banner'
+    //     src={banner}
+    //     layout='fill'
+    //     // width={1500} height={566}
+    //   />
+    // </Box>
   );
 };
 
