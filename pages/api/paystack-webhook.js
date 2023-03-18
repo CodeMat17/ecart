bfaf5b2ffa36aee99de1848c7e93a2cded703c66
CodeMat <codemat.biz@gmail.com>
2023-03-18 01:09:15 +0100

    updated

--- pages/api/paystack-webhook.js
+++ pages/api/paystack-webhook.js
@@ -11,3 +11,8 @@
 //     res.status(400).json({ message: "Invalid request method" });
 //   }
 // }
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
