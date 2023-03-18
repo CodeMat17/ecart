8df7d1357c06fd0b7a6997e5bb10bfe23c9a818d
CodeMat <codemat.biz@gmail.com>
2023-03-18 12:56:44 +0100

    updated

--- pages/api/paystack-webhook.js
+++ pages/api/paystack-webhook.js
@@ -1,18 +1,16 @@
-// export default async function handle(req, res) {
-//   if (req.query.secret !== process.env.NEXT_PUBLIC_VALIDATION_SECRET) {
-//     res.status(401).json({ message: "Invalid secret" });
-//   }
-//   if (req.method === "POST") {
-//     const event = req.body;
-//     // Process the Paystack event here
-//     console.log(event);
-//     res.status(200).json({ received: true });
-//   } else {
-//     res.status(400).json({ message: "Invalid request method" });
-//   }
-// }
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handle(req, res) {
+  console.log(req.query.secret);
+  res.status(200).json({ received: "secret received" });
 }
+
+// if (req.query.secret !== process.env.NEXT_PUBLIC_VALIDATION_SECRET) {
+//   res.status(401).json({ message: "Invalid secret" });
+// }
+// if (req.method === "POST") {
+//   const event = req.body;
+//   // Process the Paystack event here
+//   console.log(event);
+//   res.status(200).json({ received: true });
+// } else {
+//   res.status(400).json({ message: "Invalid request method" });
+// }
